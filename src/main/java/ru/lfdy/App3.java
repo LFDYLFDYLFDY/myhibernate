b805fa06de12f1e4d722ff6e5bfbbca6799744a8
LFDY <lfdy@yandex.ru>
2024-06-10 16:50:35 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/lfdy/App3.java
+++ src/main/java/ru/lfdy/App3.java
@@ -5,9 +5,17 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.lfdy.entity.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+
 public class App3 {
-    public static void main(String[] args) {
-        work();
+    public static void main(String[] args) throws IOException{
+        //work();
+        prepareData();
 
     }
 
@@ -45,6 +53,33 @@ public class App3 {
             if (session!=null){
                 session.close();
             }
+        }
+    }
+    public static void prepareData () throws IOException {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+        Session session = null;
+        try{
+            String filename="create_table_2.sql";
+            Path pathToFile = Paths.get(filename);
+            System.out.println(pathToFile);
+//      String sql = Files.lines(Paths.get("create_table_2.sql")).collect(Collectors.joining(" "));
+         String sql = Files.lines(Paths.get("create_table_2.sql")).toString();
+//            String sql = "select version()";
+        System.out.println(sql);
+           session = factory.getCurrentSession();
+           session.beginTransaction();
+//            session.createNativeQuery(sql).executeUpdate();
+
+            session.getTransaction().commit();
+
+        }
+        finally {
+//            factory.close();
+//            if (session!=null){
+//                session.close();
+//            }
         }
     }
 }
